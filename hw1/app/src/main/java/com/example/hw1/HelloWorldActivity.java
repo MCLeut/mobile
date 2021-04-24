0451ebdf46f61b914b574bc1ab30ffc00802773a
Christopher Leuthardt <christopher.leuthardt@tutanota.de>
2021-04-24 14:29:58 +0200

    Upload hw2

--- hw1/app/src/main/java/com/example/hw1/HelloWorldActivity.java
+++ hw1/app/src/main/java/com/example/hw1/HelloWorldActivity.java
@@ -1,10 +1,10 @@
-package com.gamecodeschool.helloworld;
+package com.example.hw1;
 
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
+import android.R;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
