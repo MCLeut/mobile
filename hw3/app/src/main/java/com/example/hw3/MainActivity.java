6a190a0f20444b3ae7c13aa58be243a7eb0691f2
Christopher Leuthardt <christopher.leuthardt@tutanota.de>
2021-05-04 11:57:37 +0200

    hw3 update

--- hw3/app/src/main/java/com/example/hw3/MainActivity.java
+++ hw3/app/src/main/java/com/example/hw3/MainActivity.java
@@ -1,56 +1,177 @@
 package com.example.hw3;
-
+import android.app.Activity;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements OnClickListener
+{
+    /** Called when the activity is first created. */
+
+    Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
+    Button btnAdd,btnSub,btnMul,btnDiv,btnPow,btnEqu,btnCan,btnClr;
+    EditText editText;
+    Double oldValue;
+    char chOp=' ';
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calc_layout);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        editText=(EditText) findViewById(R.id.editText);
+
+        btn0 = (Button) btn0.findViewById(R.id.button0);
+        btn1 = (Button) btn1.findViewById(R.id.button1);
+        btn2 = (Button) btn2.findViewById(R.id.button2);
+        btn3 = (Button) btn3.findViewById(R.id.button3);
+        btn4 = (Button) btn4.findViewById(R.id.button4);
+        btn5 = (Button) btn5.findViewById(R.id.button5);
+        btn6 = (Button) btn6.findViewById(R.id.button6);
+        btn7 = (Button) btn7.findViewById(R.id.button6);
+        btn8 = (Button) btn8.findViewById(R.id.button7);
+        btn9 = (Button) btn9.findViewById(R.id.button8);
+
+        btnAdd = (Button)findViewById(R.id.buttonAdd);
+        btnSub = (Button)findViewById(R.id.buttonSub);
+        btnMul = (Button)findViewById(R.id.buttonMul);
+        btnDiv = (Button)findViewById(R.id.buttonDiv);
+        btnPow = (Button)findViewById(R.id.buttonPow);
+
+        btnEqu = (Button)findViewById(R.id.buttonEqu);
+        btnCan = (Button)findViewById(R.id.buttonCan);
+        btnClr = (Button)findViewById(R.id.buttonClear);
+
+        btn0.setOnClickListener(this);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+
+        btnAdd.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
+        btnMul.setOnClickListener(this);
+        btnPow.setOnClickListener(this);
+        btnEqu.setOnClickListener(this);
+        btnCan.setOnClickListener(this);
+        btnClr.setOnClickListener(this);
+    }
+
+    public void onClick(View v)
+    {
+        try
+        {
+            Button btn = (Button)v;
+            Double answer, curValue;
+
+            switch(v.getId())
+            {
+                case R.id.button0:
+                case R.id.button1:
+                case R.id.button2:
+                case R.id.button3:
+                case R.id.button4:
+                case R.id.button5:
+                case R.id.button6:
+                case R.id.button7:
+                case R.id.button8:
+                case R.id.button9:
+
+                    editText.setText(editText.getText()+""+btn.getText()+"");
+                    break;
+
+                case R.id.buttonAdd:
+                case R.id.buttonSub:
+                case R.id.buttonMul:
+                case R.id.buttonDiv:
+                case R.id.buttonPow:
+
+                    if(editText.getText()+"" == "")
+                    {
+                        return;
+                    }
+                    chOp=(btn.getText()+"").charAt(0);
+                    oldValue=Double.parseDouble(editText.getText()+"");
+                    editText.setText("");
+                    break;
+
+                case R.id.buttonCan:
+
+                    oldValue=0.0;
+                    editText.setText("");
+                    break;
+
+                case R.id.buttonClear:
+
+                    int len = (editText.getText()+"").length();
+                    if(len==0)
+                    {
+                        Toast.makeText(getApplicationContext(), "Nothing to erase...", Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        editText.setText((editText.getText()+"").substring(0, len-1));
+                        break;
+                    }
+
+
+
+                case R.id.buttonEqu:
+
+                    if(editText.getText()+"" == "")
+                    {
+                        return;
+                    }
+                    curValue = Double.parseDouble(editText.getText()+"");
+                    switch (chOp)
+                    {
+                        case '+':
+
+                            answer = oldValue+curValue;
+                            editText.setText(answer+"");
+                            break;
+
+                        case '-':
+
+                            answer=oldValue-curValue;
+                            editText.setText(answer+"");
+                            break;
+
+                        case '*':
+
+                            answer=oldValue*curValue;
+                            editText.setText(answer+"");
+                            break;
+
+                        case '/':
+
+                            answer=oldValue/curValue;
+                            editText.setText(answer+"");
+                            break;
+
+                        case '^':
+
+                            answer=Math.pow(oldValue, curValue);
+                            editText.setText(answer+"");
+                            break;
+                    }
             }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        catch (Exception e)
+        {
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+        }
     }
 }
